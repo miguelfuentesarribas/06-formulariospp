27f970911929e4ba1d2a9685c33709231dcd365c
Miguel <miguel.fuentes@bosonit.com>
2022-05-25 14:57:27 +0200

    inicio seccion 16

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { TemplateModule } from './template/template.module';
+
+const routes: Routes = [
+    {
+        path: 'reactive',
+        loadChildren: () => import('./reactive/reactive.module')
+                            .then( m => m.ReactiveModule)
+    },
+    {
+        path: 'template',
+        loadChildren: () => import('./template/template.module')
+                            .then( m => m.TemplateModule)
+    },
+    {
+        path: '**',
+        redirectTo: 'template'
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule { }
